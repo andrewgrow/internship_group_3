112c6b04e002db6b646b101779929cba6aa0a6ee
Andrew Gahov <andrew.gahov@gmail.com>
2023-01-07 11:09:00 +0200

    describe swagger document and add to User controller swagger decorators

--- src/users/dto/update.dto.ts
+++ src/users/dto/update.dto.ts
@@ -6,24 +6,61 @@ import {
   Length,
 } from 'class-validator';
 import { User } from '../users.interface';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateUserDto implements Partial<User> {
+  @ApiProperty({
+    description: 'The email of a user. Not will update when empty.',
+    minimum: 5,
+    maximum: 100,
+    type: String,
+    required: false,
+    nullable: true,
+    example: 'user@example.com',
+  })
   @IsOptional()
   @Length(5, 100)
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: 'The first name of a user. Not will update when empty.',
+    minimum: 2,
+    maximum: 200,
+    type: String,
+    required: false,
+    nullable: true,
+    example: 'John',
+  })
   @IsNotEmpty()
   @IsOptional()
   @IsString()
   @Length(2, 20)
   firstName?: string;
 
+  @ApiProperty({
+    description: 'The last name of a user. Not will update when empty.',
+    minimum: 2,
+    maximum: 200,
+    type: String,
+    required: false,
+    nullable: true,
+    example: 'Doe',
+  })
   @IsOptional()
   @IsString()
   @Length(2, 20)
   lastName?: string;
 
+  @ApiProperty({
+    description: 'The password of a user. Not will update when empty.',
+    minimum: 8,
+    maximum: 1024,
+    type: String,
+    required: false,
+    nullable: true,
+    example: 'Qwerty78',
+  })
   @IsOptional()
   @IsString()
   @Length(8, 1024)
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,28 +9,76 @@ import {
   Post,
   UsePipes,
   ValidationPipe,
+  HttpCode,
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create.dto';
 import { User } from './users.interface';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update.dto';
+import { ApiBody, ApiTags, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('Users')
 @Controller('/users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Get()
+  @ApiResponse({
+    status: 200,
+    description: 'The records have been successfully found.',
+  })
   get(): User[] {
     return this.usersService.getAll();
   }
 
   @Post()
+  @ApiBody({ type: CreateUserDto })
+  @ApiResponse({
+    status: 201,
+    description: 'The record has been successfully created.',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad request. Check model arguments.',
+  })
+  @ApiResponse({
+    status: 409,
+    description: 'Email is already taken. Set other email or log in.',
+  })
+  @HttpCode(201)
   @UsePipes(ValidationPipe)
   create(@Body() createUserDto: CreateUserDto): User {
     return this.usersService.createUser(createUserDto);
   }
 
+  @Get('/:id')
+  @ApiResponse({
+    status: 200,
+    description: 'The record has been successfully found.',
+  })
+  @ApiResponse({
+    status: 404,
+    description: `User with id not found.`,
+  })
+  @UsePipes(ValidationPipe)
+  getUserById(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.getUserById(id);
+  }
+
   @Patch('/:id')
+  @ApiBody({ type: UpdateUserDto })
+  @ApiResponse({
+    status: 200,
+    description: 'The record has been successfully patched.',
+  })
+  @ApiResponse({
+    status: 418,
+    description: `User with id not found. Will using "I am a teapot" exception as a joke (because we can).`,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad request. Check model arguments.',
+  })
   @UsePipes(ValidationPipe)
   update(
     @Param('id', ParseIntPipe) id: number,
@@ -40,13 +88,15 @@ export class UsersController {
   }
 
   @Delete('/:id')
+  @ApiResponse({
+    status: 200,
+    description: 'The record has been successfully deleted.',
+  })
+  @ApiResponse({
+    status: 418,
+    description: `User with id not found. Will using "I am a teapot" exception as a joke (because we can).`,
+  })
   delete(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.deleteUser(id);
-  }
-
-  @Get('/:id')
-  @UsePipes(ValidationPipe)
-  getUserById(@Param('id', ParseIntPipe) id: number) {
-    return this.usersService.getUserById(id);
   }
 }
