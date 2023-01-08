b0c5ac473cfffd927cd5c5fff129c3eeda6afb2b
Andrew Gahov <andrew.gahov@gmail.com>
2023-01-08 10:05:31 +0200

    add encrypt decrypt password

--- src/users/users.schema.ts
+++ src/users/users.schema.ts
@@ -1,5 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
+import * as bcrypt from 'bcrypt';
 
 export type UserDocument = HydratedDocument<User>;
 
@@ -16,6 +17,31 @@ export class User {
 
   @Prop({ required: true })
   password: string;
+
+  /**
+   * Check is decrypted password match to encrypted password at a model.
+   * See an implementation method below.
+   */
+  isPasswordValid: (decryptedPassword: string) => boolean;
 }
 
 export const UsersSchema = SchemaFactory.createForClass(User);
+
+/**
+ * Encrypt password during save model.
+ */
+UsersSchema.pre<UserDocument>('save', async function (next) {
+  const user = this as UserDocument;
+  user.password = await bcrypt.hash(user.password, 10);
+  next();
+});
+
+/**
+ * Check is decrypted password match to encrypted password at a model.
+ * Here is an implementation of the method.
+ */
+UsersSchema.methods.isPasswordValid = async function (
+  decryptedPassword: string,
+): Promise<boolean> {
+  return await bcrypt.compare(decryptedPassword, this.password);
+};
